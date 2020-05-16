b688677f13d490755a6b64089408e26cd18c6e63
duypham9669 <44285940+duypham9669@users.noreply.github.com>
2020-05-16 18:34:45 +0700

    first demo bot

--- NewsFeedVn/Controllers/HomeController.cs
+++ NewsFeedVn/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using NewsFeedVn.service;
+using Quartz;
+using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,7 +14,23 @@ namespace NewsFeedVn.Controllers
     {
         public ActionResult Index()
         {
+            RunProgramRunExample();
             return View();
+        }
+        private static async Task RunProgramRunExample()
+        {
+            IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+            IJobDetail job = JobBuilder.Create<bot1>().Build();
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                  (s =>
+                     s.WithIntervalInSeconds(10)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0, 0))
+                  )
+                .Build();
+            scheduler.ScheduleJob(job, trigger);
         }
 
         public ActionResult About()
