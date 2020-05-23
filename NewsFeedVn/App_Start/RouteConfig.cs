b72a903550bf04430a1fd8c11dea419512c9aabc
hieula312 <44285860+hieula312@users.noreply.github.com>
2020-05-23 16:32:20 +0700

    Add CRUD and seeding for category

--- NewsFeedVn/App_Start/RouteConfig.cs
+++ NewsFeedVn/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace NewsFeedVn
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "Admin/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
