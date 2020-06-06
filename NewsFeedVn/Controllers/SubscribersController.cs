15429708a13b13b36fe58eb5c9ca9aa217f7bf96
hieula312 <44285860+hieula312@users.noreply.github.com>
2020-06-06 17:50:22 +0700

    crud for Q&A, subscriber and fix format for sources,articles also update layout backend

---
+++ NewsFeedVn/Controllers/SubscribersController.cs
@@ -0,0 +1,47 @@
+﻿using NewsFeedVn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsFeedVn.Controllers
+{
+    public class SubscribersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(db.Subscribers.ToList());
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subscriber subscriber = db.Subscribers.Find(id);
+            if (subscriber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subscriber);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Subscriber subscriber = db.Subscribers.Find(id);
+            db.Subscribers.Remove(subscriber);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
